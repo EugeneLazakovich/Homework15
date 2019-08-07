b9967538aa5f1266b057de1e31a657980a215e9c
EugeneLazakovich <zlazakovich@gmail.com>
2019-08-07 17:24:12 +0100

    Homework, Lazakovich

---
+++ 15.1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp27
+{
+    class Shared
+    {
+        public Boolean flag = true;
+        public void OneCust(string name)
+        {
+            Console.WriteLine(name + "'s buying hamburger");
+            Thread.Sleep(3000);
+        }
+        public void isClose()
+        {
+            int rnd = new Random().Next(0, 9);
+            if (rnd < 3)
+            {
+                Console.WriteLine("Mac is closed");
+                flag = false;
+            }
+        }
+    }
+    class Customer
+    {
+        public string name;
+        Semaphore sem;
+        Shared shared;
+        public Customer(string name, Shared shared, Semaphore sem)
+        {
+            this.name = name;
+            this.shared = shared;
+            this.sem = sem;
+            new Thread(MyMethod).Start();
+        }
+        public void MyMethod()
+        {
+            sem.WaitOne();
+            if (!shared.flag)
+            {
+                sem.Release();
+            }
+            else
+            {
+                shared.OneCust(name);
+                shared.isClose();
+                sem.Release();
+            }            
+        }
+    }
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Shared shared = new Shared();
+            Semaphore sem = new Semaphore(1, 1);
+            new Customer("John", shared, sem);
+            new Customer("Elvis", shared, sem);
+            new Customer("Vasya", shared, sem);
+            new Customer("Caren", shared, sem);
+            new Customer("Lusien", shared, sem);                 
+        }
+    }
+}
---
+++ 15.2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace _15._2
+{
+    class WorkerProducer
+    {
+        public int MethodProducer(int count)
+        {
+            return count;
+        }
+    }
+    class Worker
+    {
+        string name;
+        int count;
+        public int countWork = 0;
+        WorkerProducer workProd;
+        public Worker(WorkerProducer workProd, string name, int count)
+        {
+            this.workProd = workProd;
+            this.name = name;
+            this.count = count;
+            new Thread(addWorker).Start();
+        }
+        public override string ToString()
+        {
+            return name + count + ": " + countWork + " golds";
+        }
+        public void addWorker()
+        {
+            workProd.MethodProducer(count);
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                countWork += 3;
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Worker> workList = new List<Worker>();
+            int count = 1;
+            Boolean flag = true;
+            int countGold = 1000;
+            WorkerProducer workProd = new WorkerProducer();
+            workList.Add(new Worker(workProd, "Worker ", count++));
+            workList.Add(new Worker(workProd, "Worker ", count++));
+            workList.Add(new Worker(workProd, "Worker ", count++));
+            while (countGold > 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    foreach (Worker list in workList)
+                    {
+                        Console.WriteLine(list);
+                        countGold = countGold - 3;
+                        if (countGold < 0)
+                        {
+                            flag = false;
+                        }
+                    }                    
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    if (!flag) { break; }
+                }
+                if (!flag) { break; }
+                workList.Add(new Worker(workProd, "Worker ", count++));
+            }
+            foreach (Worker list in workList)
+            {
+                Console.WriteLine(list);
+            }
+        }
+    }
+}
